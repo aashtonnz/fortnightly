a750a993e85f1b5cf9453a26badb7d6a1d68b29b
Andrew Ashton <aashtonnz@gmail.com>
2019-04-30 21:55:54 +1200

    Add nav bar

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Layout from './layout/layout'
 
 const app = () => {
   return (
-    <div>
-    </div>
+    <Layout>
+
+    </Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
-import './index.css';
-import App from './app';
+import App from './app/app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({ typography: { useNextVariants: true } });
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector("#root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+      
